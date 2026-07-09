fd8fe77ad0c9d3e1ba0727d8a72fe6c1c33ec70e
exeoter <exeoter@getdroomer.online>
2026-07-09 18:27:23 +0200

    Add files via upload

---
+++ modules/constant.lua
@@ -0,0 +1,5 @@
+local common = game:GetService("CoreGui").RobloxGui.Modules.Common
+local commonutil = common.CommonUtil
+local constants = commonutil:Clone()
+constants.Name = "Constants"
+constants.Parent = common
